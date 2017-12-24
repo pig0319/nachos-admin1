57550b88674b727717d82f631fee80b5aeafb9f5
phinix101 <34806816+phinix101@users.noreply.github.com>
2017-12-24 16:56:32 +0800

    Homepage.mp

---
+++ homepage.php
@@ -0,0 +1,541 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>Naughty Nachos</title> 
+	<meta name="description" content="Essentia Responsive Bootstrap Theme by Bootstrap Master"/>
+	<meta name="keywords" content="Template, Theme, web, html5, css3, Bootstrap" />
+	<meta name="author" content="Łukasz Holeczek from creativeLabs"/>
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: Facebook Open Graph -->
+	<meta property="og:title" content=""/>
+	<meta property="og:description" content=""/>
+	<meta property="og:type" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content=""/>
+	<!-- end: Facebook Open Graph -->
+
+    <!-- start: CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/slider1.css" rel="stylesheet">
+	<link href="css/slider2.css" rel="stylesheet">
+	<link href="css/slider3.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+	<!-- end: CSS -->
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	
+	<!--start: Header -->
+	<header>
+		
+		<!--start: Container -->
+		<div class="container">
+						
+			<!--start: Row -->
+			<div class="row-fluid">
+					
+				<!--start: Navigation -->
+				<div class="navigation"> 
+				
+					<div class="navbar navbar-fixed-top">
+			    		<div class="navbar-inner">
+			          		<a class="btn btn-navbar btnOverlay" data-toggle="collapse" data-target=".nav-collapse">
+				            		menu
+			          		</a>
+			          		<div class="nav-collapse collapse">
+			            		<ul class="nav">
+									<li><a href="#" class="home">Home</a></li>
+									<li><a href="#" class="menu">Featured</a></li>
+									<li><a href="#" class="about">About</a></li>
+									<li><a href="#" class="services">Services</a></li>
+									<li><a href="#" class="contact">Contact</a></li>
+			            		</ul>
+			          		</div>
+			        	</div>
+			      	</div>
+				
+				
+				</div>	
+				<!--end: Navigation -->	
+					
+			</div>
+			<!--end: Row -->
+			
+		</div>
+		<!--end: Container-->			
+			
+	</header>
+	<!--end: Header-->
+	<!-- start: 1st Page - Home -->
+	<div id="home">
+
+		<div class="demo-1">
+
+	        <div id="slider" class="sl-slider-wrapper">
+
+				<div class="sl-slider">
+
+					<div class="sl-slide bg-1" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+						<div class="sl-slide-inner">
+							<div class="deco" data-icon="N"></div>
+							<h2>Naughty Nachos</h2>
+							<blockquote><p>You have just dined, and however scrupulously the slaughterhouse is concealed in the graceful distance of miles, there is complicity.</p></blockquote>
+						</div>
+					</div>
+
+					<div class="sl-slide bg-2" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+						<div class="sl-slide-inner">
+							<div class="deco" data-icon="b"></div>
+							<h2>Best Sellers</h2>
+							<blockquote><p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p></blockquote>
+
+						</div>
+					</div>
+
+					<div class="sl-slide bg-3" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="sl-slide-inner">
+							<div class="deco" data-icon="R"></div>
+							<h2>Reservation</h2>
+							<blockquote><p>We offer reservations</p><button type = "button">reserve</button></blockquote>
+						</div>
+					</div>
+
+					<div class="sl-slide bg-4" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="sl-slide-inner">
+							<div class="deco" data-icon="I"></div>
+							<h2>Donna nobis pacem</h2>
+							<blockquote><p>The human body has no more need for cows' milk than it does for dogs' milk, horses' milk, or giraffes' milk.</p><cite>Michael Klaper M.D.</cite></blockquote>
+						</div>
+					</div>
+
+					<div class="sl-slide bg-5" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="sl-slide-inner">
+							<div class="deco" data-icon="t"></div>
+							<h2>Acta Non Verba</h2>
+							<blockquote><p>I think if you want to eat more meat you should kill it yourself and eat it raw so that you are not blinded by the hypocrisy of having it processed for you.</p><cite>Margi Clarke</cite></blockquote>
+						</div>
+					</div>
+				</div><!-- /sl-slider -->
+
+				<nav id="nav-arrows" class="nav-arrows">
+					<span class="nav-arrow-prev">Previous</span>
+					<span class="nav-arrow-next">Next</span>
+				</nav>
+
+				<nav id="nav-dots" class="nav-dots">
+					<span class="nav-dot-current"></span>
+					<span></span>
+					<span></span>
+					<span></span>
+					<span></span>
+				</nav>
+
+			</div><!-- /slider-wrapper -->
+
+	    </div>
+
+	</div>
+	<!-- end: 1st Page - Home -->
+	
+	
+	
+	<!-- start: 3rd Page - Pricing Tables -->
+	<div id="menu" class="color blue">
+		
+		<!--start: Container -->
+		<div class="container">
+
+			<!--start: Wrapper -->
+			<div class="wrapper span12">
+
+							<!-- start: Page Title -->
+			<div id="page-title">
+
+				<div id="page-title-inner">
+
+					<h2><span>Featured Items</span></h2>
+
+					<br>
+					<br>
+
+						<div class ="daily-special">
+							<div class="left-pic"><img src="file:///C:/Users/neo/Documents/Module%20Ni%20Blaise/img/Naught%20Nachos%20Images/1.jpg"></div>
+							<div class="details">  
+								<h1>Daily Specials</h1>
+								<h3>We serve a new special everyday. Check our Facebook page and see what we're having today!</h3>
+									<br>
+
+								<div class = "lower-details">
+									<p>FOLLOW US ON FACEBOOK TO SEE OUR DAILY SPECIALS IN YOUR FEED!</p>
+								</div>
+								
+							</div>
+						</div>
+
+					<br>
+
+						<div class="flex-container">
+							<div class ="box"><img src="file:///C:/Users/neo/Documents/Module%20Ni%20Blaise/img/Naught%20Nachos%20Images/sub1.jpg">
+									<h3>Something Something</h3>
+							</div>
+							<div class ="box"><img src="file:///C:/Users/neo/Documents/Module%20Ni%20Blaise/img/Naught%20Nachos%20Images/sub3.jpg">
+									<h3>Something Something</h3>
+							</div>
+							<div class ="box"><img src="file:///C:/Users/neo/Documents/Module%20Ni%20Blaise/img/Naught%20Nachos%20Images/sub2.jpg">
+									<h3>Something Something</h3>
+							</div>
+							
+						</div>
+
+					
+
+						<div class="flex-container">
+							<div class ="buybox">
+								<a href="menu.html" class="buybutton">SEE MENU</a>
+							</div>
+							<div class ="buybox">
+								<a href="menu.html" class="buybutton">SEE MENU</a>
+							</div>
+							<div class ="buybox">
+								<a href="google.com" class="buybutton">SEE MENU</a>
+							</div>
+						</div>
+							
+
+				</div>	
+
+			</div>
+			<!-- end: Page Title -->
+				
+			<!-- start: Table -->
+			
+			<!-- end: Table -->
+			</div>
+			<!-- end: Wrapper -->
+		
+		</div>
+		<!--end: Container -->
+		
+	</div>
+	<!-- end: 3rd Page - Pricing Tables -->
+	
+	<!-- start: 4th Page - Portfolio -->
+	<div id="portfolio" class="color white">
+		
+		<!--start: Container -->
+		<div class="container">
+
+			
+		
+		</div>
+		<!--end: Container -->
+		
+	</div>
+	<!-- end: 4th Page - Portfolio -->
+	
+	<!-- start: 5th Page - About -->
+	<div id="about" class="color white">
+		
+		<!--start: Container -->
+		<div class="container">
+
+			<!--start: Wrapper -->
+			<div class="wrapper span12">
+
+							<!-- start: Page Title -->
+			<div id="page-title">
+
+				<div id="page-title-inner">
+
+						<h2><span>About Us</span></h2>
+
+				</div>	
+
+			</div>
+			<!-- end: Page Title -->				
+		
+			<!--start: Row -->
+	    	<div class="row-fluid">
+		
+				<div class="span12">
+					
+					<!-- start: About Us -->
+					<div id="story">
+						<h3>Our Story</h3>
+						<p>
+							Naughty Nachos is located in Magiting st. corner Maginhawa Quezon City. You can easily spot us, just right next to ministop. See you here!
+						</p>
+						
+					</div>	
+					<!-- end: About Us -->
+
+					
+
+				</div>
+				
+			</div>
+			<!--end: Row -->
+			
+			<!-- start: Row -->
+			<div class="row-fluid">		
+				
+				
+				
+			</div>
+			<!--end: Row -->
+			</div>
+			<!-- end: Wrapper -->
+		
+		</div>
+		<!--end: Container -->
+		
+	</div>
+	<!-- end: 5th Page - About -->
+	
+	<!-- start: 6th Page - Reservation -->
+	<div id="Reservation-container">
+		<h1>BOOK A TABLE</h1>
+		<p>If you would like to book at table at Naughty Nachos, please use our booking form below.</p>
+		<p>We will send you comfirmation as soon as we can.</p>
+
+		<p>For bookings less than 48 hours away please call us on 0969696969</p>
+		<p>Bookings by phone only for Maginhawa area residence.</p>
+		
+	</div>
+	<!-- End: 6th Page - Reservation -->
+	<div class="menu_txt">
+
+<div class = "flex-container">
+    <div class = "flexy">
+        <figure class="snip1418"><img src="img14.jpg"/></figure>
+    </div>
+    <div class = "flexy">
+        <figure class="snip1418"><img src="img15.jpg"/></figure>
+    </div>
+    <div class = "flexy">
+        <figure class="snip1418"><img src="img16.jpg"/></figure>
+    </div>
+    <div class = "flexy">
+        <figure class="snip1418"><img src="img18.jpg"/></figure>  
+    </div>
+</div>
+
+	<!-- start: 7th Page - Contact -->
+	<div id="mapBg"></div>
+	<div id="contact" class="color blue transparent">
+		
+		<!--start: Container -->
+		<div class="container">
+
+			<!--start: Wrapper -->
+			<div class="wrapper span12">
+
+				
+	
+			<!-- start: Page Title -->
+			<div id="page-title">
+
+				<div id="page-title-inner">
+
+						<h2><span>Contact</span></h2>
+
+				</div>	
+
+			</div>
+			<!-- end: Page Title -->
+			
+			<!-- start: Row -->
+			<div class="row-fluid">		
+				
+				<div class="span12">
+					
+					<!-- start: Contact Info -->
+					<b>Naughty Nachos</b> •
+					95 Magiting Cor. Maginhawa •
+					Quezon City, MNL, PH •
+					facebook: facebook.com/naughtnachos	
+					<!-- end: Contact Info -->
+				
+				</div>	
+			
+			</div>
+			<!-- end: Row -->
+			
+			<hr class="clean">
+			
+			<!-- start: Row -->
+			<div class="row-fluid">		
+			
+				<!-- start: Map -->
+				<div class="span6">
+					
+					<!-- start: Contact Form -->
+					<div id="contact-form">
+
+						<form method="post" action="">
+							
+							<fieldset>
+								
+								<input tabindex="4" id="name" name="name" type="text" value="" class="span12" placeholder="Name: Your Name Here" >
+								<input tabindex="2" id="email" name="email" type="text" value="" class="span12" placeholder="Email: email@gmail.com">
+								<textarea tabindex="3" class="input-xlarge span12" id="message" name="body" rows="7" placeholder="Message: Your message here"></textarea>
+
+								<div class="actions">
+									<button tabindex="2" type="submit" class="btn btn-succes btn-large">Send message</button>
+								</div>
+								
+							</fieldset>
+
+						</form>
+
+					</div>
+					<!-- end: Contact Form -->
+					
+
+				</div>
+				<!-- end: Map -->
+				
+				<div class="span6">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.1646643695194!2d121.05706081428035!3d14.646592479875872!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b773776f4fa9%3A0x2e979d5efe70dae!2sNaughty+Nachos!5e0!3m2!1sen!2sph!4v1506188056700" width=100% height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+				
+				</div>	
+
+			
+			</div>
+			<!-- end: Row -->
+
+
+			</div>
+			<!-- end: Wrapper -->
+		
+		</div>
+		<!--end: Container -->
+		
+	</div>
+	<!-- end: 7th Page - Contact -->
+
+	
+	<footer>
+		
+		<div id="copyright">
+			Naughty Nachos Est. 2014 &copy 
+		</div>
+		<div id="social">
+			<a href="http://facebook.com/naughtnachos" rel="tooltip" title="Facebook" class="facebook">Facebook</a>
+			
+		</div>	
+		
+	</footer>	
+
+<!-- start: Java Script -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/isotope.js"></script>
+<script type="text/javascript" src="js/jquery.imagesloaded.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/flexslider.js"></script>
+<script type="text/javascript" src="js/carousel.js"></script>
+<script type="text/javascript" src="js/fancybox.js"></script>
+<script type="text/javascript" src="js/twitter.js"></script>
+<script type="text/javascript" src="js/modernizr.custom.79639.js"></script>
+<script type="text/javascript" src="js/jquery.ba-cond.min.js"></script>
+<script type="text/javascript" src="js/jquery.slitslider.js"></script>
+
+<script type="text/javascript" src="js/excanvas.js"></script>
+<script type="text/javascript" src="js/jquery.flot.min.js"></script>
+<script type="text/javascript" src="js/jquery.flot.pie.min.js"></script>
+<script type="text/javascript" src="js/jquery.flot.stack.js"></script>
+<script type="text/javascript" src="js/jquery.flot.resize.min.js"></script>
+
+<script defer="defer" src="js/custom.js"></script>
+<script type="text/javascript">	
+	$(function() {
+	
+		var Page = (function() {
+
+			var $navArrows = $( '#nav-arrows' ),
+				$nav = $( '#nav-dots > span' ),
+				slitslider = $( '#slider' ).slitslider( {
+					onBeforeChange : function( slide, pos ) {
+
+						$nav.removeClass( 'nav-dot-current' );
+						$nav.eq( pos ).addClass( 'nav-dot-current' );
+
+					}
+				} ),
+
+				init = function() {
+
+					initEvents();
+					
+				},
+				initEvents = function() {
+
+					// add navigation events
+					$navArrows.children( ':last' ).on( 'click', function() {
+
+						slitslider.next();
+						return false;
+
+					} );
+
+					$navArrows.children( ':first' ).on( 'click', function() {
+						
+		  				slitslider.previous();
+						return false;
+
+					} );
+
+					$nav.each( function( i ) {
+					
+						$( this ).on( 'click', function( event ) {
+							
+							var $dot = $( this );
+							
+							if( !slitslider.isActive() ) {
+
+								$nav.removeClass( 'nav-dot-current' );
+								$dot.addClass( 'nav-dot-current' );
+							
+							}
+							
+							slitslider.jump( i + 1 );
+							return false;
+						
+						} );
+						
+					} );
+
+				};
+
+				return { init : init };
+
+		})();
+
+		Page.init();
+	
+	});
+</script>
+<!-- end: Java Script -->
+
+</body>
+</html>
